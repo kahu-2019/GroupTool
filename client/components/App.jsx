1849bc95dc087562de9343b8135e5c27c00ed780
Richard Graham <richgrahamdev@gmail.com>
2019-02-01 11:16:17 +1300

    toggle button working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import List from './List'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
-const App = () => {
+import List from './List'
+import Button from './Sorted'
+
+
+const TestComp = () => {
   return (
     <div>
-
-      <List />
-
+      <h1>testComp</h1>
     </div>
   )
+}
+
+const App = () => {
+    return (
+      <Router>
+        <div>
+          <h1>Group Selector</h1>
+          <div>
+            <Route path="/" component={List} />
+            <Route path="/" component={Button}/>
+          </div>
+        </div>
+      </Router>
+    )
 }
 
 export default App
--- client/components/List.jsx
+++ client/components/List.jsx
@@ -8,38 +8,8 @@ const List = () => {
     var randomIndex = Math.floor(Math.random() * x.length)
     return (
         < div >
-            <h1>List of Cohort</h1>
             <ul>
                 {data.map(element => <li>{element.name}</li>)}
-                <br />
-
-                <div>
-
-                    <li>{x[randomIndex]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <br />
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <br />
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <br />
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <br />
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
-
-
-
-                </div>
-
-
             </ul>
         </div >)
 }
