e9a44aac3ce46cc855708819372ae79cf45c7dc9
ranjit03 <ranjitendurthi03@gmail.com>
2019-01-31 20:24:53 +1300

    random selection working

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -4,25 +4,46 @@ import data from '../../data/people'
 
 
 const List = () => {
-    console.log(data)
+    var x = data.map(element => element.name)
     return (
         < div >
             <h1>List of Cohort</h1>
             <ul>
                 {data.map(element => <li>{element.name}</li>)}
+                <br />
+
+                <div>
+
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <br />
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <br />
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <br />
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <br />
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+                    <li>{x[Math.floor(Math.random() * x.length)]}</li>
+
+
+
+                </div>
+
+
             </ul>
         </div >)
 }
 
 
-// export default function List() {
-//     return (
-//         < div >
-//             <h1>List of Cohort</h1>
-//             <ul>
-//                 {x.name}
-//             </ul>
-//         </div >)
-// }
+
 
 export default List
